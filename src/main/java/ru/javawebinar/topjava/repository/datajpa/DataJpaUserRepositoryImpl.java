ff57b54e470fe2d8c7ede2b7bd8bd5298e413361
travov <bycorvusrus@gmail.com>
2018-05-04 11:24:53 +0300

    HW8_Optional_add_transactional

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaUserRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaUserRepositoryImpl.java
@@ -3,6 +3,7 @@ package ru.javawebinar.topjava.repository.datajpa;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
 
@@ -46,6 +47,7 @@ public class DataJpaUserRepositoryImpl implements UserRepository {
     }
 
     @Override
+    @Transactional
     public void changeEnable(boolean isEnabled, int id) {
         User user = get(id);
         user.setEnabled(isEnabled);
